575d3eaa7f2e586d75c4f30781dec73cca4f8d94
齋藤大輔 <saitoudaisuke@saitoudaisukenoMacBook-Air.local>
2020-01-26 15:24:45 +0900

    add response message

--- index.js
+++ index.js
@@ -11,8 +11,32 @@ const line_config = {
 // Webサーバーの設定
 server.listen(process.env.PORT || 3000);
 
+// APIコールのためのクライアントインスタンスを作成
+const bot = new link.Client(line_config);
+
 // ルーターの設定
 server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
+  // 先行してLINE側に200を送る
   res.sendStatus(200);
-  console.log(rew.body);
+  // 全てのイベント処理のプロミスを格納する配列
+  let events_processed = [];
+  // イベントオブジェクトを順次処理
+  req.body.events.forEach((event) => {
+    //イベントのタイプがメッセージかつテキストだったら
+    if(event.type == 'message' && event.message.type == 'text') {
+      if(event.message.text == 'こんにちは'){
+        // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+        events_processed.push(bot.replyMessage(event.replyToken, {
+          type: 'text',
+          text: 'これはこれは'
+        }));
+      }
+    }
+  });
+  // 全てのイベント処理が終了したら何個のイベントが処理されたかを出力
+  Promise.all(events_processed).then(
+    (response) => {
+      console.log(`${response.length} event(s) processed`);
+    }
+  )
 });
