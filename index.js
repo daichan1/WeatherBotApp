348d6478a2facf3280e4da6ba2b2dfbfb333173b
齋藤大輔 <saitoudaisuke@saitoudaisukenoMacBook-Air.local>
2020-02-11 10:49:56 +0900

    modified message_return

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const server = require("express")();
 const line = require("@line/bot-sdk"); // Messaging APIをインポート
 const weather = require("./api/weather");
+const axios = require("axios");
 
 // パラメーターの設定(LINE)
 const line_config = {
@@ -26,12 +27,37 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     //イベントのタイプがメッセージかつテキストだったら
     if(event.type == 'message' && event.message.type == 'text') {
       if(event.message.text == 'こんにちは'){
-        const resData = weather.forecastData();
-        // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-        events_processed.push(bot.replyMessage(event.replyToken, {
-          type: 'text',
-          text: resData
-        }))
+        // const resData = weather.forecastData();
+        axios.get(apiUrl, {
+          params: {
+            id: cityId,
+            appid: process.env.OPEN_WEATHER_API_APPID
+          },
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+          },
+          responseType: 'json'
+        })
+        .then(res => {
+          // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+          events_processed.push(bot.replyMessage(event.replyToken, {
+            type: 'text',
+            text: "APIの実行に成功"
+          }))
+        })
+        .catch(err => {
+          // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+          events_processed.push(bot.replyMessage(event.replyToken, {
+            type: 'text',
+            text: "APIの実行に失敗"
+          }))
+        })
+        // // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+        // events_processed.push(bot.replyMessage(event.replyToken, {
+        //   type: 'text',
+        //   text: resData
+        // }))
       }
     }
   });
