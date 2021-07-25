3f2780248d5a5c3e58080eaf82120cd0a428d4f3
Daisuke Saito <chansa-89.one-line@ezweb.ne.jp>
2021-07-25 18:41:34 +0900

    add_kelvin_to_celsius

--- index.js
+++ index.js
@@ -74,15 +74,20 @@ function responseMessage(daily_data) {
   return `
 日付：${unixtimeToDate(daily_data.dt)}
 天気：${daily_data.weather[0].main}
-最高気温：${daily_data.temp.max}
-最低気温：${daily_data.temp.min}
-降水確率：${daily_data.pop}
+最高気温：${kelvinToCelsius(daily_data.temp.max)}度
+最低気温：${kelvinToCelsius(daily_data.temp.min)}度
+降水確率：${daily_data.pop * 100}%
   `
 }
 
 // unix時間の変換
 function unixtimeToDate(unixtime) {
-  const options = { year: 'numeric', month: 'long', day: 'numeric' };
   let date = new Date(unixtime * 1000)
   return `${date.getFullYear()}年${date.getMonth()+1}月${date.getDate()}日`
+}
+
+// 気温の変換
+function kelvinToCelsius(kelvin) {
+  const kelvinDegree = 273.15
+  return kelvin - kelvinDegree
 }
