450b2ad12215f4543a5a9fdf8df659328d38edd3
Daisuke Saito <chansa-89.one-line@ezweb.ne.jp>
2021-07-25 14:04:29 +0900

    response_week_weather

--- index.js
+++ index.js
@@ -42,9 +42,21 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
         })
         .then(res => {
           // 返信内容を設定してユーザーに送信
+          let week_weather = ""
+          for(i = 0; i < res.daily.length; i++) {
+            let one_day_weather = `
+              日付：${res.daily[i].dt}
+              天気：${res.daily[i].weather[0].main}
+              最高気温：${res.daily[i].temp.max}
+              最低気温：${res.daily[i].temp.min}
+              降水確率：${res.daily[i].pop}
+              ¥n
+              `
+              week_weather += one_day_weather
+          }
           events_processed.push(bot.replyMessage(event.replyToken, {
             type: 'text',
-            text: '週間予報を出力'
+            text: week_weather
           }))
           
         })
